f5860e896ebfa0e7b7838dfa2b2e250de60c4900
AnnFyz <annfyz1232@yandex.ru>
2023-01-04 17:36:25 +0100

    add navMesh

--- ProceduralMazes/Assets/Scripts/Maze/MazeSpawner.cs
+++ ProceduralMazes/Assets/Scripts/Maze/MazeSpawner.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
+using Unity.AI.Navigation;
 
 public class MazeSpawner : MonoBehaviour
 {
     public Cell CellPrefab;
     public Vector3 CellSize = new Vector3(1,1,0);
     public HintRenderer HintRenderer;
+    NavMeshSurface surface;
 
     public Maze maze;
 
     private void Start()
+    {
+        surface = GetComponent<NavMeshSurface>();
+        SpawnMaze();
+        surface.BuildNavMesh();
+    }
+
+    void SpawnMaze()
     {
         MazeGenerator generator = new MazeGenerator();
         maze = generator.GenerateMaze();
@@ -18,7 +27,7 @@ public class MazeSpawner : MonoBehaviour
             for (int y = 0; y < maze.cells.GetLength(1); y++)
             {
                 Cell c = Instantiate(CellPrefab, new Vector3(x * CellSize.x, y * CellSize.y, y * CellSize.z), Quaternion.identity);
-
+                c.transform.parent = this.transform;
                 c.WallLeft.SetActive(maze.cells[x, y].WallLeft);
                 c.WallBottom.SetActive(maze.cells[x, y].WallBottom);
             }
